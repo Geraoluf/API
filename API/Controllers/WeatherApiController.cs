707e6852134f421aa3012d6bea5fd747b51da556
gert Rasmussen <pjente@gmail.com>
2025-09-14 15:13:13 +0200

    API

---
+++ API/Controllers/WeatherApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]                         
+    [Route("api/[controller]")]               
+    public class WeatherApiController : ControllerBase
+    {
+        [HttpGet("temperature")]            
+        public IActionResult GetTemperature()
+        {
+           
+            var temperature = new
+            {
+                Station = "06149",
+                Value = 17.3,
+                Unit = "°C"
+            };
+
+            return Ok(temperature);         
+        }
+    }
+}
